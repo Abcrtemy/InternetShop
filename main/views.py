fbf6a4bb33d2b23484fa8d63a0951266c250a437
Abcrtemy <ahrenellla@gmail.com>
2024-03-06 17:18:46 +0500

    template tag for catalog

--- main/views.py
+++ main/views.py
@@ -8,11 +8,10 @@ from goods.models import Categories
 
 #представление или контроллер
 def index (request):
-    categories = Categories.objects.all()
+    
     context = {
         'title': 'Home - Главная ',
         'content' : 'Магазин мебели Home',
-        'categories' : categories
     }
     return render(request, 'main/index.html',context)
 
